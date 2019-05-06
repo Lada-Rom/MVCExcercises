d8845fb200f00cef4d4b58fe855f25938d209d25
LS <asdfg@gmail.com>
2019-05-06 16:35:13 +0300

    +Excercise1

---
+++ Excercise1/Excercise1/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Excercise1.Controllers
+{
+    public class ProductController : Controller
+    {
+        //
+        // GET: /Product/
+
+        public ActionResult Index()
+        {
+            List<Models.Product> products = new List<Models.Product>();
+
+            products.Add(new Models.Product(1, "Pen", 10, "Pen"));
+            products.Add(new Models.Product(2, "Eraser", 20, "Eraser"));
+            products.Add(new Models.Product(3, "Pencil", 30, "Pencil"));
+
+            return View(products);
+        }
+
+    }
+}
---
+++ Excercise1/Excercise1/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Excercise1.Models
+{
+    public class Product
+    {
+        public int ID { get; set;}
+        public string Name { get; set;}
+        public decimal Price { get; set;}
+        public string Description { get; set;}
+
+        public Product(int id, string name, decimal price, string description)
+        { 
+            ID = id;
+            Name = name;
+            Price = price;
+            Description = description;
+        }
+
+        public Product()
+        {
+            // TODO: Complete member initialization
+        }
+    }
+}
