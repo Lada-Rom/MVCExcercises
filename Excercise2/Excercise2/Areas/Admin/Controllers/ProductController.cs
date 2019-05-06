f32e418e3770173ff5984c07e31c7686aa70e4bf
LS <asdfg@gmail.com>
2019-05-06 16:35:49 +0300

    +Excercise2

---
+++ Excercise2/Excercise2/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Excercise2.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        //
+        // GET: /Admin/Product/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+    }
+}
