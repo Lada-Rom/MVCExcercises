11a4b8a38f56aa45d2466ba9e3eeb723f019cb10
LS <asdfg@gmail.com>
2019-11-06 18:24:13 +0300

    [MSVC] Project "DynArray"

---
+++ prj.lab/dynarray/dynarray.h
@@ -0,0 +1,150 @@
+// Copyright 2019 by Tolstenko Lada under Free Public License 1.0.0
+
+#ifndef DYNARRAY_DYNARRAY_H_20190611
+#define DYNARRAY_DYNARRAY_H_20190611
+
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+namespace tolstenko_l_s {
+
+template<typename T>
+class DynArray {
+public:
+    using iterator = T*;
+    using const_iterator = const T*;
+
+    DynArray() = default;
+    DynArray(size_t, const T& = T());
+    DynArray(const DynArray&);
+    DynArray(DynArray&&);
+    DynArray(std::initializer_list<T>);
+
+    ~DynArray() { delete [] data_; }
+
+    size_t Size() const { return size_; }
+    bool Empty() const { return size_ == 0; }
+
+    iterator begin() { return data_; }
+    const_iterator begin() const { return data_; }
+
+    iterator end() { return data_ + size_; }
+    const_iterator end() const { return data_ + size_; }
+
+    void Resize (size_t, const T& = T());
+
+    T& operator [] (size_t i) { return data_[i]; }
+    const T& operator [] (size_t i) const { return data_[i]; }
+
+    DynArray& operator = (const DynArray&);
+    DynArray& operator = (DynArray&&);
+
+private:
+    size_t capacity_{0};
+    size_t size_{0};
+    T* data_{nullptr};
+};
+
+template<typename T>
+bool operator == (const DynArray<T>&, const DynArray<T>&);
+
+template<typename T>
+bool operator != (const DynArray<T>&, const DynArray<T>&);
+
+////////////////////////////////////////////////////////////////////////////////
+
+template<typename T>
+DynArray<T>::DynArray(size_t size, const T& value) 
+    : size_(size)
+    , capacity_(size)
+    , data_(new T[size]) {
+
+    for (size_t i = 0; i < size_; ++i) {
+        data_[i] = value;
+    }
+}
+
+template<typename T>
+DynArray<T>::DynArray(const DynArray& other)
+    : size_(other.size_)
+    , capacity_(other.size_)
+    , data_(new T[other.size_]) {
+
+    std::copy(other.begin(), other.end(), data_);
+}
+
+template<typename T>
+DynArray<T>::DynArray(DynArray&& other)
+    : size_(other.size_)
+    , capacity_(other.size_)
+    , data_(other.data_) {
+
+    other.size_ = 0;
+    other.capacity_ = 0;
+    other.data_ = nullptr;
+}
+
+template<typename T>
+DynArray<T>::DynArray(std::initializer_list<T> list)
+    : size_(list.size())
+    , capacity_(list.size())
+    , data_(new T[list.size()]) {
+
+    std::copy(list.begin(), list.end(), data_);
+}
+
+template<typename T>
+DynArray<T>& DynArray<T>::operator =(const DynArray& other) {
+    if (&other != this) {
+        *this = DynArray(other);
+    }
+    return *this;
+}
+
+template<typename T>
+DynArray<T>& DynArray<T>::operator =(DynArray&& other) {
+    if (&other != this) {
+        DynArray t(other);
+        std::swap(size_, t.size_);
+        std::swap(capacity_, t.capacity_);
+        std::swap(data_, t.data_);
+    }
+    return *this;
+}
+
+template<typename T>
+void DynArray<T>::Resize(size_t size, const T& value) {
+    if(capacity_ <= size) {
+        DynArray t(size, value);
+        std::copy(begin(), end(), t.data_);
+        operator = (std::move(t));
+    }
+    else if(size > size_) {
+        std::fill(data_ + size_, data_ + size, value);
+    }
+    size_ = size;
+}
+
+template<typename T>
+bool operator == (const DynArray<T>& lhs, const DynArray<T>& rhs) {
+    if (lhs.Size() != rhs.Size())
+        return false;
+
+    for (size_t i = 0; i < lhs.Size(); ++i) {
+        if (lhs[i] != rhs[i])
+            return false;
+    }
+
+    return true;
+}
+
+template<typename T>
+bool operator != (const DynArray<T>& lhs, const DynArray<T>& rhs) {
+    return !(lhs == rhs);
+}
+
+} //namespace tolstenko_l_s
+
+#endif
---
+++ prj.test/test_dynarray/test_dynarray.cpp
@@ -0,0 +1,39 @@
+#include <dynarray/dynarray.h>
+
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.h>
+
+TEST_CASE("DynArray creation test", "[DynArrayCreation]") {
+    using tolstenko_l_s::DynArray;    DynArray<int> ar;    REQUIRE(ar.Size() == 0);    REQUIRE(ar.Empty());}TEST_CASE("DynArray constructors test", "[DynArrayConstructors]") {
+    using tolstenko_l_s::DynArray;    DynArray<int> ar(3, 42);    REQUIRE(!ar.Empty());    REQUIRE(ar.Size() == 3);    for(size_t i = 0; i < ar.Size(); ++i) {        REQUIRE(ar[i] == 42);    }    for (auto value : ar) {
+        REQUIRE(value == 42);
+    }
+}TEST_CASE("DynArray copying test", "[DynArrayCopying]") {
+    using tolstenko_l_s::DynArray;    DynArray<int> ar1(3, 42);    DynArray<int> ar2(ar1);
+    REQUIRE(ar1 == ar2);
+    REQUIRE(!(ar1 != ar2));
+
+    DynArray<int> ar3;
+
+    ar3 = ar2;
+    REQUIRE(ar3 == ar1);
+    REQUIRE(ar3 == ar2);
+}TEST_CASE("DynArray initializerList test", "[DynArrayInitializerList]") {
+    using tolstenko_l_s::DynArray;    DynArray<int> ar {0, 1, 2, 3, 4};
+
+    REQUIRE(ar.Size() == 5);
+
+    for(size_t i = 0; i < ar.Size(); ++i) {
+        REQUIRE(ar[i] == i);
+    }
+}TEST_CASE("DynArray resize test", "[DynArrayResize]") {
+    using tolstenko_l_s::DynArray;    DynArray<int> ar{ 0, 1, 2, 3, 4, 5, 6, 7 };
+
+    ar.Resize(4);
+    REQUIRE(ar.Size() == 4);
+    REQUIRE(ar == DynArray<int> {0, 1, 2, 3});
+
+    ar.Resize(7, 10);
+    REQUIRE(ar.Size() == 7);
+    REQUIRE(ar == DynArray<int> {0, 1, 2, 3, 10, 10, 10});
+}
