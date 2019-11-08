ebb46ab47ca5b250a3c35cb64ac46e01f82db016
LS <asdfg@gmail.com>
2019-11-08 21:54:59 +0300

    [MSVC] CMakeLists changed

--- prj.test/test_longint/test_longint.cpp
+++ prj.test/test_longint/test_longint.cpp
@@ -3,21 +3,21 @@
 #include <longint/longint.h>
 
 #define CATCH_CONFIG_MAIN
-#include <catch2/catch.h>
+#include <catch2/catch.hpp>
 
 TEST_CASE("LongInt tests", "[LongInt]") {
     using tolstenko_l_s::LongInt;
 
-    REQUIRE(LongInt(12345678) == 12345678);
-    REQUIRE(LongInt(-12345678) == -12345678);
+    CHECK(LongInt(12345678) == 12345678);
+    CHECK(LongInt(-12345678) == -12345678);
 
-    REQUIRE(LongInt(17 * 19 * 23 * 29) / 23 == 17 * 19 * 29);
-    REQUIRE(LongInt(17 * 19 * 23 * 29) / -23 == -17 * 19 * 29);
-    REQUIRE(LongInt(-17 * 19 * 23 * 29) / 23 == -17 * 19 * 29);
-    REQUIRE(LongInt(-17 * 19 * 23 * 29) / -23 == 17 * 19 * 29);
+    CHECK(LongInt(17 * 19 * 23 * 29) / 23 == 17 * 19 * 29);
+    CHECK(LongInt(17 * 19 * 23 * 29) / -23 == -17 * 19 * 29);
+    CHECK(LongInt(-17 * 19 * 23 * 29) / 23 == -17 * 19 * 29);
+    CHECK(LongInt(-17 * 19 * 23 * 29) / -23 == 17 * 19 * 29);
 
-    REQUIRE((LongInt(17 * 19 * 29) *= 23) == 17 * 19 * 23 * 29);
-    REQUIRE((LongInt(17 * 19 * 29) *= -23) == -17 * 19 * 23 * 29);
-    REQUIRE((LongInt(-17 * 19 * 29) *= 23) == -17 * 19 * 23 * 29);
-    REQUIRE((LongInt(-17 * 19 * 29) *= -23) == 17 * 19 * 23 * 29);
+    CHECK((LongInt(17 * 19 * 29) *= 23) == 17 * 19 * 23 * 29);
+    CHECK((LongInt(17 * 19 * 29) *= -23) == -17 * 19 * 23 * 29);
+    CHECK((LongInt(-17 * 19 * 29) *= 23) == -17 * 19 * 23 * 29);
+    CHECK((LongInt(-17 * 19 * 29) *= -23) == 17 * 19 * 23 * 29);
 }
